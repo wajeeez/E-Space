85df3d35d10bcaf6240d64fb723ad599df7a027e
wajahatalisyed <s.wajahatalisyed@gmail.com>
2023-09-10 04:30:06 +0500

    Dashboard

--- fypdesign/src/Components/Final_Design_Components/TeacherDashboard_Components/Content.js
+++ fypdesign/src/Components/Final_Design_Components/TeacherDashboard_Components/Content.js
@@ -6,13 +6,12 @@ import TDashboard from '../../../Components/Final_Design_Components/TeacherDashb
 import AssignmentPage from '../../../Pages/Teacher/Assignment/Assignment';
 import AssignmentList from '../../../Pages/Teacher/AssigmentList/AssignmentList';
 import Class from '../../../Pages/Classes/Class/Class';
-import StdAssignment from '../../../Pages/Student/Assignment/stdAssignment';
 const Content = ({ currentPage }) => {
   const pageComponents = {
     dashboard: <Class/>,
     lectures: <div>lectures</div>,
-    assignment: <StdAssignment/>,
-    // assignmentList: <AssignmentList/>,
+    assignment: <AssignmentPage/>,
+    assignmentList: <AssignmentList/>,
     quiz: <div>quiz</div>,
     account: <div>user</div>,
     class: <div>class</div>,
