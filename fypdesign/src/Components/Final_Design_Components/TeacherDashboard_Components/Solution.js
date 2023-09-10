04df40b56de31957ad0b0b9ab9c803c33f644b6a
Syed AbdulSamad <84366628+AbdulSamad675@users.noreply.github.com>
2023-09-11 03:24:11 +0500

    dash

---
+++ fypdesign/src/Components/Final_Design_Components/TeacherDashboard_Components/Solution.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import dashIcon from '../../../Assets/images/dashboard.png';
+import perfIcon from '../../../Assets/images/tre.png';
+import assignIcon from '../../../Assets/images/assign.png';
+import logoutIcon from '../../../Assets/images/logout.png';
+import accsetIcon from '../../../Assets/images/user-gear.png';
+import dataIcon from '../../../Assets/images/aud.png';
+import userIcon from '../../../Assets/images/user2.png';
+import quizIcon from '../../../Assets/images/quiz.png';
+import lectIcon from '../../../Assets/images/le.png';
+import retIcon from '../../../Assets/images/return1.png';
+import delIcon from '../../../Assets/images/del.png';
+
+import AssignmentPage from '../../../Pages/Teacher/Assignment/Assignment';
+import AssignmentList from '../../../Pages/Teacher/AssigmentList/AssignmentList';
+import Class from '../../../Pages/Classes/Class/Class';
+
+import './Solution.css';
+import './TSidebar.css';
+import './TContent.css';
+
+
+function Solution() {
+  const [currentPage, setCurrentPage] = useState('dashboard'); // Initial page
+
+  const handlePageChange = (pageName) => {
+    setCurrentPage(pageName);
+  };
+
+  return (
+    <div className="solution-container">
+      <div className="td-sidebar">
+        <div className="td-sidebar-top">
+          <div className="td-sidebar-icon">
+            <img src={userIcon} alt="Icon" className="td-sidebar-icon" />
+          </div>
+          <div className="td-sidebar-text">Wajahat</div>
+        </div>
+        <div className="td-sidebar-middle">
+          <button className="td-sidebar-button" onClick={() => handlePageChange('dashboard')}>
+            <img src={dashIcon} alt="Icon" className="td-sidebar-button-icon" />
+            Dashboard
+          </button>
+          <button className="td-sidebar-button" onClick={() => handlePageChange('performance')}>
+            <img src={perfIcon} alt="Icon" className="td-sidebar-button-icon" />
+            Performance
+          </button>
+          <button className="td-sidebar-button" onClick={() => handlePageChange('lectures')}>
+            <img src={lectIcon} alt="Icon" className="td-sidebar-button-icon" />
+            Lectures
+          </button>
+          <button className="td-sidebar-button" onClick={() => handlePageChange('class')}>
+            <img src={dataIcon} alt="Icon" className="td-sidebar-button-icon" />
+            Class Data
+          </button>
+          <button className="td-sidebar-button" onClick={() => handlePageChange('assignment')}>
+            <img src={assignIcon} alt="Icon" className="td-sidebar-button-icon" />
+            Assignment
+          </button>
+          <button className="td-sidebar-button" onClick={() => handlePageChange('assignmentList')}>
+            <img src={assignIcon} alt="Icon" className="td-sidebar-button-icon" />
+            AssignmentList
+          </button>
+          <button className="td-sidebar-button" onClick={() => handlePageChange('quiz')}>
+            <img src={quizIcon} alt="Icon" className="td-sidebar-button-icon" />
+            Quiz
+          </button>
+        </div>
+
+        <div className="td-sidebar-bottom">
+          <button className="td-sidebar-button" onClick={() => handlePageChange('return')}>
+            <img src={retIcon} alt="Icon" className="td-sidebar-button-icon" />
+            Leave Class
+          </button>
+          <button className="td-sidebar-button" onClick={() => handlePageChange('account')}>
+            <img src={accsetIcon} alt="Icon" className="td-sidebar-button-icon" />
+            User Settings
+          </button>
+          <button className="td-sidebar-button" onClick={() => handlePageChange('logout')}>
+            <img src={logoutIcon} alt="Icon" className="td-sidebar-button-icon" />
+            Logout
+          </button>
+        </div>
+
+        <div className="td-sidebar-bottom-d">
+          <button className="td-sidebar-button" onClick={() => handlePageChange('delete')}>
+            <img src={delIcon} alt="Icon" className="td-sidebar-button-icond" />
+            Delete Class <br />Permanently
+          </button>
+        </div>
+      </div>
+
+      <div className="td-content">
+        {currentPage === 'dashboard' && <Class />}
+        {currentPage === 'lectures' && <div>lectures</div>}
+        {currentPage === 'assignment' && <AssignmentPage />}
+        {currentPage === 'assignmentList' && <AssignmentList />}
+        {currentPage === 'quiz' && <div>quiz</div>}
+        {currentPage === 'account' && <div>user</div>}
+        {currentPage === 'class' && <div>class</div>}
+        {currentPage === 'performance' && <div>performance</div>}
+        {currentPage === 'return' && <div>home</div>}
+        {currentPage === 'delete' && <div>delete</div>}
+        {currentPage === 'logout' && <div>logout</div>}
+      </div>
+    </div>
+  );
+}
+
+export default Solution;
--- fypdesign/src/Pages/Final_Design/TeacherDashboard/TDashboard.jsx
+++ fypdesign/src/Pages/Final_Design/TeacherDashboard/TDashboard.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import './TDashboard.module.css';
 import TNavbar from '../../../Components/Final_Design_Components/TeacherDashboard_Components/TNavbar';
-import TSidebar from '../../../Components/Final_Design_Components/TeacherDashboard_Components/TSidebar';
-import TContent from '../../../Components/Final_Design_Components/TeacherDashboard_Components/TContent';
+// import TSidebar from '../../../Components/Final_Design_Components/TeacherDashboard_Components/TSidebar';
+// import TContent from '../../../Components/Final_Design_Components/TeacherDashboard_Components/TContent';
 import TFooter from '../../../Components/Final_Design_Components/TeacherDashboard_Components/TFooter';
+import Solution from '../../../Components/Final_Design_Components/TeacherDashboard_Components/Solution'
 
 const TDashboard = () => {
   const [currentPage, setCurrentPage] = useState('home');
@@ -20,10 +21,13 @@ const TDashboard = () => {
   return (
     <div className="td-app">
       <TNavbar onNavbarButtonClick={handleNavbarButtonClick} />
-      <div className="td-content-container">
+      {/* <div className="td-content-container">
         {sidebarVisible && <TSidebar onPageChange={handlePageChange} />}
         <TContent currentPage={currentPage} />
-      </div>
+      </div> */}
+      <Solution/>
+
+      
       <TFooter />
     </div>
   );
