f0882c6cf8f66d1ac6941d32176228c5753aee51
Syed AbdulSamad <84366628+AbdulSamad675@users.noreply.github.com>
2023-10-15 21:02:59 +0500

    carousel

--- fypdesign/src/Components/Final_Design_Components/LandingPage_Components/Carousel.js
+++ fypdesign/src/Components/Final_Design_Components/LandingPage_Components/Carousel.js
@@ -1,54 +1,121 @@
 import React, { useState } from 'react';
-import { Carousel } from 'react-bootstrap';
-import main from '../../../Assets/images/home.jpg';
+import { Container, Row, Col, Carousel, Button } from 'react-bootstrap';
+import feat from '../../../Assets/images/home.jpg';
 
-function CustomCarousel() {
-  const [activeIndex, setActiveIndex] = useState(0);
-  
-  const cards = [
-    { title: 'Card Title 1', description: 'This is the description for card 1' },
-    { title: 'Card Title 2', description: 'This is the description for card 2' },
-    { title: 'Card Title 3', description: 'This is the description for card 3' },
-    { title: 'Card Title 4', description: 'This is the description for card 4' },
-    { title: 'Card Title 5', description: 'This is the description for card 5' },
-    { title: 'Card Title 6', description: 'This is the description for card 6' },
-  ];
+import './Carousel.css';
 
-  const itemsPerPage = 3;
+const cardData = [
+  {
+    imgSrc: feat,
+    title: 'Card 1',
+    description: 'Description for Card 1',
+    link: '/card1',
+  },
+  {
+    imgSrc: feat,
+    title: 'Card 2',
+    description: 'In the updated code, I modified the transition property to apply the transition to all properties, including background-color. This change ensures a smoother transition for the background color change on hover.',
+    link: '/card2',
+  },
+  {
+    imgSrc: feat,
+    title: 'Card 3',
+    description: 'Description for Card 3',
+    link: '/card3',
+  },
+  {
+    imgSrc: feat,
+    title: 'Card 4',
+    description: 'Description for Card 4',
+    link: '/card4',
+  },
+  {
+    imgSrc: feat,
+    title: 'Card 5',
+    description: 'Description for Card 5',
+    link: '/card5',
+  },
+  {
+    imgSrc: feat,
+    title: 'Card 6',
+    description: 'Description for Card 6',
+    link: '/card6',
+  },
+  {
+    imgSrc: feat,
+    title: 'Card 7',
+    description: 'Description for Card 7',
+    link: '/card7',
+  },
+];
 
-  const handleSelect = (selectedIndex, e) => {
-    setActiveIndex(selectedIndex);
+const CardsCarousel = () => {
+  const [activeRow, setActiveRow] = useState(0);
+ 
+
+  const handleRowChange = (newRow) => {
+    setActiveRow(newRow);
   };
 
-  const renderCards = () => {
-    const startIndex = activeIndex * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const visibleCards = cards.slice(startIndex, endIndex);
-
-    return visibleCards.map((card, index) => (
-      <div className="col-md-4" key={index}>
-        <div className="card">
-          <img src={main} className="card-img-top" alt="Card Image" />
-          <div className="card-body">
+  const renderCards = (start, end) => {
+    const cards = cardData.slice(start, end);
+    return cards.map((card, index) => (
+      <Col key={index} md={4}>
+        <div className="card " 
+        onClick={() => (card.link)}
+        style={{ borderRadius: '20px' , border: 'none',  backgroundColor: ''}}
+        >
+          <img src={card.imgSrc} alt={card.title} />
+          <div className="card-body ">
             <h5 className="card-title">{card.title}</h5>
             <p className="card-text">{card.description}</p>
           </div>
         </div>
-      </div>
+      </Col>
     ));
   };
 
-  return (
-    <Carousel activeIndex={activeIndex} onSelect={handleSelect} interval={null} indicators={false}>
-      <Carousel.Item>
-        <div className="container">
-          <div className="row">
-            {renderCards()}
-          </div>
-        </div>
-      </Carousel.Item>
-    </Carousel>
-  );
-}
+ 
 
-export default CustomCarousel;
+  return (
+    <Container>
+      <Row className="mt-3">
+        <Col>
+          <Carousel activeIndex={activeRow} indicators={false} controls={false}>
+            <Carousel.Item>
+              <Row>{renderCards(0, 3)}</Row>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Row>{renderCards(3, 6)}</Row>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Row>{renderCards(6, 7)}</Row>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col className="d-flex justify-content-center">
+          <div className="custom-buttons">
+            <Button
+              onClick={() => handleRowChange(activeRow - 1)}
+              disabled={activeRow === 0}
+              className="btn-transition"
+            >
+              Prev
+            </Button>
+            <Button
+              onClick={() => handleRowChange(activeRow + 1)}
+              disabled={activeRow === 2}
+              className="btn-transition"
+            >
+             Next
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default CardsCarousel;
