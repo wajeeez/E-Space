41673c0eb7e3e39e5efd166510b0196315091b64
Syed AbdulSamad <84366628+AbdulSamad675@users.noreply.github.com>
2023-10-17 21:30:17 +0500

    register

--- fypdesign/src/Components/Final_Design_Components/LandingPage_Components/HeroSection.js
+++ fypdesign/src/Components/Final_Design_Components/LandingPage_Components/HeroSection.js
@@ -1,44 +1,181 @@
-
-
-// import { useNavigate } from "react-router";
-// const navigate = useNavigate()
-
-// src/components/HeroSection.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import './HeroSection.css';
 import main from '../../../Assets/images/home.jpg';
+import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+
+import { Reg } from '../../../api/internal';
+
+import { setUser } from '../../../store/userSlice';
+
+import { useNavigate } from 'react-router';
+import { useDispatch } from 'react-redux';
 
 function HeroSection() {
+  const [showRegistrationCard, setShowRegistrationCard] = useState(false);
+  const [registrationComplete, setRegistrationComplete] = useState(false);
+  const [error, setError] = useState('');
+
+  const toggleRegistrationCard = () => {
+    setShowRegistrationCard(!showRegistrationCard);
+  };
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleReg = async () => {
+    const data = {
+      tname: values.tname,
+      institute: values.institute,
+      phone: values.phone,
+      email: values.email,
+      password: values.password,
+    };
+
+    try {
+      const response = await Reg(data);
+
+      if (response.status === 409) {
+        setError(response.response.data.message);
+        setRegistrationComplete(false);
+      } else if (response.status === 201) {
+        const teacher = {
+          _id: response.data.teacher._id,
+          email: response.data.teacher.email,
+          auth: response.data.teacher.auth,
+        };
+
+        dispatch(setUser(teacher));
+        setRegistrationComplete(true);
+        // navigate('/teacher/login');
+      } else if (response.code === 'ERR_BAD_REQUEST') {
+        setError(response.response.data.message);
+        setRegistrationComplete(false);
+      }
+    } catch (error) {
+      console.error(error);
+      setRegistrationComplete(false);
+    }
+  };
+
+  const { values, touched, handleBlur, handleChange, errors } = useFormik({
+    initialValues: {
+      tname: '',
+      institute: '',
+      phone: '',
+      email: '',
+      password: '',
+    },
+    // validationSchema: regSchema, // You can add your validation schema here
+  });
+
   return (
-    <div className='hero-container'>
-      <div className='hcontent'>
-        <h1 className='htitle'>Welcome to E-Space</h1>
-        <p className='hdescription'>Empowering Education, Empowering You: Choose E-Space</p>
-        <button className='hjoin-button'>Join now</button>
+    <div className="hero-container">
+      <div className="hcontent">
+        <h1 className="htitle">Welcome to E-Space</h1>
+        <p className="hdescription">Empowering Education, Empowering You: Choose E-Space</p>
+        <button className="hjoin-button" onClick={toggleRegistrationCard}>
+          Join now
+        </button>
       </div>
-      {/* <div className='himage-container'>
-        <img src={main} alt='E-Space' />
-      </div> */}
+
+      {showRegistrationCard && !registrationComplete && (
+        <div className="container mt-5 custom-container">
+          <div className="row justify-content-center">
+            <div className="col-md-6">
+              <div className="custom-card" style={{ borderRadius: '20px', border: '3px solid black', padding: '10px', backgroundColor: '' }}>
+                <div className="card-body">
+                  <h2 className="custom-title text-center">Register as Teacher</h2>
+
+                  <div className="form-group custom-form-group">
+                    <input
+                      type="text"
+                      value={values.tname}
+                      name="tname"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      className="form-control"
+                      placeholder="Teacher Name"
+                    />
+                  </div>
+
+                  <div className="form-group custom-form-group">
+                    <input
+                      type="text"
+                      value={values.institute}
+                      name="institute"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      className="form-control"
+                      placeholder="Institute"
+                    />
+                  </div>
+
+                  <div className="form-group custom-form-group">
+                    <input
+                      type="text"
+                      value={values.phone}
+                      name="phone"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      className="form-control"
+                      placeholder="Phone"
+                    />
+                  </div>
+
+                  <div className="form-group custom-form-group">
+                    <input
+                      type="text"
+                      value={values.email}
+                      name="email"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      className="form-control"
+                      placeholder="Email"
+                    />
+                  </div>
+
+                  <div className="form-group custom-form-group">
+                    <input
+                      type="password"
+                      name="password"
+                      value={values.password}
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      className="form-control"
+                      placeholder="Password"
+                    />
+                  </div>
+
+                  <button className="btn btn-primary btn-block custom-button"
+                    onClick={handleReg}
+                    style={{ borderRadius: '20px', fontSize: 'large', backgroundColor: 'blue' }}
+                  >
+                    Register
+                  </button>
+
+                  {error !== '' ? (
+                    <div className="alert alert-danger mt-3">{error}</div>
+                  ) : null}
+
+                  <p className="text-center mt-3">Already have an account?</p>
+                  <Link to="/teacher/login">Login here</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {registrationComplete && (
+        <div className="registration-complete" style={{ textAlign: 'center' }}>
+          <i className="fa fa-check-circle" style={{ color: 'green', fontSize: '50px' }}></i>
+          
+          <p style={{ color: 'green', fontSize: '50px', fontFamily: 'Raleway' , }}>Registration Complete</p>
+        </div>
+      )}
     </div>
   );
 }
 
-
 export default HeroSection;
-
-
-
-
-
-
-
-
-/*
-<video src='/videos/video-2.mp4' autoPlay loop muted />
-*/
-
-
-
-
-
--- fypdesign/src/Components/Final_Design_Components/LandingPage_Components/Signin_Options.js
+++ fypdesign/src/Components/Final_Design_Components/LandingPage_Components/Signin_Options.js
@@ -1,58 +1,130 @@
+// import React from 'react';
+// import './Signin_Options.css';
+
+
+// import Titems from './Titems';
+
+// function Signin_Options() {
+//   return (
+//     <div className='signin-container'>
+//       <section className='signin-subscription'>
+//         <p className='signin-subscription-heading'>
+//           Sign In As ...
+//         </p>
+//         {/* <p className='signin-subscription-text'>
+//           "Unleash the Power of E-Space: Empowering First-Time Users with Exceptional Tutorials, Fostering Profound Understanding of Every Function. Discover the Full Potential of Our Software for an Inspirational Journey of Educational Excellence!"
+//         </p> */}
+
+//       </section>
+
+//       <section className='tcards'>
+//         <div className='t__container'>
+//           <div className='t__wrapper'>
+//             <ul className='t__items'>
+//               <Titems
+//                 src='..\images\t.png'
+//                 text='Teacher'
+
+//                 path='/teacher/login'
+//               />
+//               <Titems
+//                 src='..\images\s2.png'
+//                 text='Student'
+
+//                 path='/std/login'
+//               />
+//             </ul>
+//           </div>
+//         </div>
+//       </section>
+      
+//     </div>
+//   );
+// }
+
+// export default Signin_Options;
+
+import tea from '../../../Assets/images/t1.png';
+import std from '../../../Assets/images/w1.png';
+import './Signin_Opt.css';
 import React from 'react';
-import './Signin_Options.css';
+import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
 
+const titleStyle = {
+  fontSize: '30px',
+  fontWeight: 'bold',
+  fontFamily: 'Raleway',
+  marginTop: '20px',
+  letterSpacing: '1px',
 
-import Titems from './Titems';
+};
 
-function Signin_Options() {
+const centerStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  
+};
+
+const header = {
+  fontSize: '60px',
+  fontWeight: 'bold',
+  fontFamily: 'Raleway',
+  marginBottom: '-10px',
+  letterSpacing: '1px',
+};
+
+const containerStyle = {
+  height: '100vh',
+  maxHeight: '100vh',
+ 
+};
+
+const SigninOptions = () => {
   return (
-    <div className='signin-container'>
-      <section className='signin-subscription'>
-        <p className='signin-subscription-heading'>
-          Sign In As ...
-        </p>
-        {/* <p className='signin-subscription-text'>
-          "Unleash the Power of E-Space: Empowering First-Time Users with Exceptional Tutorials, Fostering Profound Understanding of Every Function. Discover the Full Potential of Our Software for an Inspirational Journey of Educational Excellence!"
-        </p> */}
-
-      </section>
-
-      <section className='tcards'>
-        <div className='t__container'>
-          <div className='t__wrapper'>
-            <ul className='t__items'>
-              <Titems
-                src='..\images\t.png'
-                text='Teacher'
-
-                path='/teacher/login'
+    <div style={containerStyle}  className="container">
+      <h1 style={header} className="text-center mt-4">Sign In As</h1>
+      <div style={centerStyle} className="row mt-4">
+        <div className="col-md-5">
+          <Link to="/teacher/login" className="text-decoration-none">
+            <div
+              className="card custom-scard"
+              style={{ borderRadius: '30px', margin: '20px' }}
+            >
+              <img
+                src={tea}
+                className="card-img-top rounded-top"
+                alt="Card Image"
               />
-              <Titems
-                src='..\images\s2.png'
-                text='Student'
-
-                path='/std/login'
+              <div className="card-body text-center">
+                <h5 className="c-title" style={titleStyle}>
+                  Teacher
+                </h5>
+              </div>
+            </div>
+          </Link>
+        </div>
+        <div className="col-md-5">
+          <Link to="/std/login" className="text-decoration-none">
+            <div
+              className="card custom-scard"
+              style={{ borderRadius: '30px', margin: '20px' }}
+            >
+              <img
+                src={std}
+                className="card-img-top rounded-top"
+                alt="Card Image"
               />
-            </ul>
-          </div>
+              <div className="card-body text-center">
+                <h5 className="c-title" style={titleStyle}>
+                  Student
+                </h5>
+              </div>
+            </div>
+          </Link>
         </div>
-      </section>
-      <center>
-      <section class='social-media'>
-        <div class='social-media-wrap'>
-
-          <div class='footer-logo'>
-          
-              <img src="../images/logo1.png" />
-              <p class='website-rights'>E-SPACE © 2023</p>
-            
-          </div>
-
-        </div>
-      </section>
-      </center>
+      </div>
     </div>
   );
-}
+};
 
-export default Signin_Options;
+export default SigninOptions;
