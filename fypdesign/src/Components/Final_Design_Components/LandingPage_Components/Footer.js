fd13cbda2b860aa27db62ef54c529961118871cb
Syed AbdulSamad <84366628+AbdulSamad675@users.noreply.github.com>
2023-12-20 20:12:12 +0500

    landing page fixes

--- fypdesign/src/Components/Final_Design_Components/LandingPage_Components/Footer.js
+++ fypdesign/src/Components/Final_Design_Components/LandingPage_Components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../../Assets/images/logo1.png';
 import gplay from '../../../Assets/images/playstore2.png';
 import aplay from '../../../Assets/images/appstore1.png';
@@ -7,6 +7,18 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  const updateWindowSize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowSize);
+    return () => {
+      window.removeEventListener('resize', updateWindowSize);
+    };
+  }, []);
 
   const textItemStyle = {
     margin: '5px 0',
@@ -20,46 +32,94 @@ function Footer() {
 
   const paragraphStyle = {
     fontWeight: 400, 
-    fontFamily: 'Raleway, sans-serif', 
+    fontFamily: 'Poppons, sans-serif', 
     fontSize: '20px',
   };
 
   return (
-    <footer className="bg-black text-white">
-      <div className="container p-4">
+    <footer className="container-fluid bg-black text-white p-3" >
+      
+        {isMobile ? (
+        // Mobile Layout
+        <>
         <div className="row">
-          <div className="col-md-3 d-flex flex-column align-items-center">
-            <div>
-              <img src={logo} alt="Logo" style={{ width: '200px', height: 'auto', marginBottom: '30px' }} className="logo-img" />
+          <div className="col flex-column text-center mb-3" style={{width:'100px'}}>
+            <h5 style={headingStyle}>Company</h5>
+            <div style={textItemStyle}>About Us</div>
+          </div>
+          <div className="col flex-column text-center mb-3" style={{width:'100px'}}>
+            <h5 style={headingStyle}>Contact us</h5>
+            <div style={textItemStyle}>Email Us</div>
+            <div style={textItemStyle}>Feedback</div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col flex-column text-center mb-2" style={{width:'100px'}}>
+            <h5 style={headingStyle}>Learn</h5>
+            <div style={textItemStyle}>Privacy</div>
+            <div style={textItemStyle}>Terms</div>
+            <div style={textItemStyle}>FAQs</div>
+          </div>
+
+          <div className="col " style={{width:'100px'}}>
+            <div className="d-flex flex-column align-items-center">
+              <img src={logo} alt="Logo" style={{ width: '150px', height: 'auto', marginBottom: '10px' }} className="logo-img" />
             </div>
             <div className="d-flex flex-column align-items-center">
             <Link to="/">
               <img
                 src={gplay}
                 alt="store"
-                style={{ width: '150px', height: '60px', marginBottom: '10px' }}
+                style={{ width: '120px', height: '40px', marginBottom: '10px' }}
               />
               </Link>
               <Link to="/">
               <img
                 src={aplay}
                 alt="store"
-                style={{ width: '150px', height: '50px', marginBottom: '20px' }}
+                style={{ width: '100px', height: '30px', marginBottom: '20px' }}
               />
               </Link>
             </div>
           </div>
+
+        </div>
+          </>
+      ) : (
+        // Desktop Layout
+        <div className="row">
+
+          <div className="col-md-3 ">
+            <div className="d-flex flex-column align-items-center">
+              <img src={logo} alt="Logo" style={{ width: '200px', height: 'auto', marginBottom: '10px' }} className="logo-img" />
+            </div>
+            <div className="d-flex flex-column align-items-center">
+            <Link to="/">
+              <img
+                src={gplay}
+                alt="store"
+                style={{ width: '140px', height: '50px', marginBottom: '10px' }}
+              />
+              </Link>
+              <Link to="/">
+              <img
+                src={aplay}
+                alt="store"
+                style={{ width: '120px', height: '40px', marginBottom: '20px' }}
+              />
+              </Link>
+            </div>
+
+          </div>
+
           <div className="col-md-3 d-flex flex-column align-items-center">
             <h5 style={headingStyle}>Company</h5>
-            <div style={textItemStyle}>About</div>
-            <div style={textItemStyle}>Blogs</div>
+            <div style={textItemStyle}>About Us</div>
           </div>
           <div className="col-md-3 d-flex flex-column align-items-center">
-            <h5 style={headingStyle}>Contact us</h5>
-            <div style={textItemStyle}>Career</div>
-            <div style={textItemStyle}>About</div>
-            <div style={textItemStyle}>How we Work</div>
-            <div style={textItemStyle}>Career</div>
+            <h5 style={headingStyle}>Contact Us</h5>
+            <div style={textItemStyle}>Email Us</div>
+            <div style={textItemStyle}>Feedback</div>
           </div>
           <div className="col-md-3 d-flex flex-column align-items-center">
             <h5 style={headingStyle}>Learn</h5>
@@ -67,13 +127,16 @@ function Footer() {
             <div style={textItemStyle}>Terms</div>
             <div style={textItemStyle}>FAQs</div>
           </div>
+
         </div>
+      )}
+
         <div className="row" style={{ borderTop: '4px solid white', paddingTop: '1rem' }}>
           <div className="col-md-12 text-center">
-            <p style={paragraphStyle}>Espace © 2023 All Rights Reserved.</p>
+            <p style={paragraphStyle}>E-SPACE © 2023 All Rights Reserved.</p>
           </div>
         </div>
-      </div>
+
     </footer>
   );
 }
